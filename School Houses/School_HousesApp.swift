a566854252a98a389f98c1f6bcdd57eea521066c
Haneen Alhmoudi <haneen.alhammoudi@gmail.com>
2023-08-20 05:48:54 +0400

    Initial Commit

---
+++ School Houses/School_HousesApp.swift
@@ -0,0 +1,17 @@
+//
+//  School_HousesApp.swift
+//  School Houses
+//
+//  Created by Haneen Al hammoudi on 20/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct School_HousesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
