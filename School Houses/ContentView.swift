5c2f0b2122b54f6d5146b23ca0421953cf8ad988
Haneen Alhmoudi <haneen.alhammoudi@gmail.com>
2023-09-09 16:30:40 +0400

    i fixed the first page ( not it look more neat )

--- School Houses/ContentView.swift
+++ School Houses/ContentView.swift
@@ -9,45 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-       
-            VStack {
-                
-            // Picture of Logo
-             Image("logo")
-            .resizable()
-            .scaledToFit()
-            .frame(width:300, height:300 )
-                
-                
-                    // sign up button
-                    Text("        Sign Up         ")
-                        .bold()
-                        .font(.largeTitle)
-                        . padding(4)
-                        .foregroundColor(.white)
-                        .background(.blue)
-                        .cornerRadius(100)
-                        .shadow(color: .gray,radius: 10, x:0, y: 10)
-                        .padding()
-                    
-                    
-                    // login button
-                    Text("        Login in         ")
-                        .bold()
-                        .font(.largeTitle)
-                        . padding(4)
-                        .foregroundColor(.yellow)
-                        .background(.white)
-                        .cornerRadius(100)
-                        .shadow(color: .gray,radius: 10, x:0, y: 10)
-            
-                }
-                
-            }
-       
-    }
+        ZStack {
+            Color.orange.opacity(0.1) // Set a transparent background
 
+//            LinearGradient(colors: [.white, .yellow.opacity(0.5), .white],
+//                           startPoint: .topLeading, endPoint: .bottomTrailing)
+            
+                .edgesIgnoringSafeArea(.all) // Extend the gradient to cover the safe area
+
+            VStack {
+                // Picture of Logo
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 800, height: 300)
+                    .padding( )
+                    .offset(x:0, y:-85)
+                
+                
+                
+                // sign up button
+                Text("         Sign Up         ")
+                    .bold()
+                    .font(.title)
+                    .padding(10)
+                    .foregroundColor(.white)
+                    .background(Color.yellow)
+                    .cornerRadius(100)
+                    .shadow(color: .gray.opacity(0.3), radius: 10, x: 0, y: 10)
+                    .padding()
+                    .offset(x:0,y:-50)
+
+                // login button
+                Text("             log in           ")
+                    .bold()
+                    .font(.title)
+                    .padding(10)
+                    .foregroundColor(.yellow)
+                    .background(Color.white)
+                    .cornerRadius(100)
+                    .shadow(color: .gray, radius: 10, x: 0, y: 10)
+                    .offset(x:0,y:-22)
+
+            }
+        }
+
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
